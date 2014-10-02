7676ab605ea53612ed31ec3ca6cc6e9c2429992e
valgreen01 <alvane.01@gmail.com>
2014-10-01 22:59:03 -0500

    Footer and quotes rotator in home page

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+$( function() {
+
+	// Quotes rotator
+	if ( $('.quotes').length )
+	{
+		$('#cbp-qtrotator').cbpQTRotator({
+				speed    : 500,
+				easing   : 'ease',
+				interval : 7000
+		});
+		
+		//$( '#cbp-qtrotator' ).cbpQTRotator( 'destroy' );
+	}
+
+
+} );
