2de591b1246e3d2ac7d4af56a45121adfb8c24e8
valgreen01 <alvane.01@gmail.com>
2014-10-22 22:36:44 -0500

    Advances Integrha website

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,35 @@
-$( function() {
+$( function()
+{
 
 	// Quotes rotator
-	if ( $('.quotes').length )
+	if ( $( '.quotes' ).length )
 	{
-		$('#cbp-qtrotator').cbpQTRotator({
+		$( '#cbp-qtrotator' ).cbpQTRotator({
 				speed    : 800,
 				easing   : 'ease',
 				interval : 7000
 		});
-		
 		//$( '#cbp-qtrotator' ).cbpQTRotator( 'destroy' );
 	}
 
+	// Info Selected Square
+	if ( $( '.square-container' ).length)
+	{
+		$( '.tabbed-elems > ul > li' ).each( function()
+		{
+			$(this).click( function()
+	        {
+	        	if ( $( this ).hasClass( 'active' ) )
+	        	{
+					$( this ).siblings().removeClass( 'active' );
+				}
+				else
+				{
+					$( this ).addClass( 'active' ).siblings().removeClass( 'active' );
+				}
+	            $( '.tabbed-elems > div' ).hide().removeClass( 'animated fadeInLeft' ).eq( $( this ).index() ).show().addClass( 'animated fadeInLeft' );
+	        });
+		}).eq( 0 ).click();
+	}
 
-} );
+});
